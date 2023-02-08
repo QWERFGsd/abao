d1b16d5e913bf84f6e99694feb111c1a9c330703
QWERFGsd <110270164+QWERFGsd@users.noreply.github.com>
2023-02-08 16:34:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-uuidone=0e8cc52e-caf5-44aa-90c1-697e49fd8887
-uuidtwo=56a697d8-3fcd-4cf0-859c-9ab1b5df58df
-uuidthree=fbcfc0ba-76ce-4fb6-a61a-ed239b70b802
-uuidfour=50da9c1f-bf7a-47fa-b933-f7ad52161d41
-uuidfive=281204fe-1c43-4d9d-8727-b2ba90fc9863
-mypath=/file-myfile
+uuidone=83aedd53-85f8-4f89-8283-e4ec96595134
+uuidtwo=a9323c95-c49c-4740-91d4-c9fb06ee2250
+uuidthree=cfac9e71-9496-4f27-a8fe-b0fc1468a810
+uuidfour=4ff2e19d-8dee-4256-b403-73315a11a6d7
+uuidfive=f770a8a2-07f3-41c0-8803-d74d9b839528
+mypath=/vless
 myport=8080
 
 
